939337b8021cb35295ba983b136f08a3d4a3abf4
N SACHIN <nsachingoud@gmail.com>
2024-08-14 11:19:53 +0530

    first commit

---
+++ 2.js
@@ -0,0 +1,26 @@
+// let random =Math.random()
+// let fn = prompt("enter the first number:")
+// let sn = prompt("Enter the second number:")
+// let op = prompt("Enter the operation:")
+// function goodcalc(a,b){
+//    if(op=="+"){
+//       console.log(a+b)
+//    }
+//    else if(op=='-'){
+//       console.log(a-b)
+//    }
+//    else if(op=="*"){
+//       console.log(a*b)
+//    }
+//    else if(op="/"){
+//       console.log(a/b)
+//    }
+   
+// }
+// if(random>0.1){
+//    console.log(goodcalc(fn,sn))
+// }
+// else{
+//    console.log("falut operation")
+// }
+
